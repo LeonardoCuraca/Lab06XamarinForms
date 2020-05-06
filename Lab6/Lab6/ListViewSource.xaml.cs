41b9417368e485778b0de685ec7f591e1e25c39c
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-05-05 20:58:13 -0500

    first commit

---
+++ Lab6/Lab6/ListViewSource.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Lab06;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewSource : ContentPage
+    {
+        public ObservableCollection<GroupedSuperHeroModel> grouped { get; set;  }
+        public ListViewSource()
+        {
+            InitializeComponent();
+            grouped = new ObservableCollection<GroupedSuperHeroModel>();
+            var marvelHeroes = new GroupedSuperHeroModel() { Comic = "Marvel" };
+            marvelHeroes.Add(new SuperHeroModel() { Name = "Iron Man", AlterEgo = "Tony Stark" });
+            marvelHeroes.Add(new SuperHeroModel() { Name = "Captain America", AlterEgo = "Steve Rogers" });
+            marvelHeroes.Add(new SuperHeroModel() { Name = "Thor God of Thunder", AlterEgo = "Thor Odinson" });
+            var dcHeroes = new GroupedSuperHeroModel() { Comic = "DC" };
+            dcHeroes.Add(new SuperHeroModel() { Name = "Batman", AlterEgo = "Bruce Wayne" });
+            dcHeroes.Add(new SuperHeroModel() { Name = "Superman", AlterEgo = "Kal-El" });
+            dcHeroes.Add(new SuperHeroModel() { Name = "Wonder Woman", AlterEgo = "Princess Diana" });
+            grouped.Add(marvelHeroes);
+            grouped.Add(dcHeroes);
+            lstView.ItemsSource = grouped;
+        }
+    }
+}
---
+++ Lab6/Lab6/SuperHeroModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Lab06
+{
+	public class SuperHeroModel : ObservableCollection<SuperHeroModel>
+	{
+		public string Name { get; set; }
+		public string AlterEgo { get; set; }
+		public SuperHeroModel()
+		{
+		}
+	}
+
+	public class GroupedSuperHeroModel : ObservableCollection<SuperHeroModel>
+	{
+		public string Comic { get; set; }
+	}
+}
